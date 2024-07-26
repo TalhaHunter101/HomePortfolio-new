555bd337b3094ecf9cf8f14c07eb8834a7396d6d
Chandrapalsingh <74819484+Chandrapalsingh12@users.noreply.github.com>
2024-07-26 21:59:52 +0530

    Added Elasticsearch

---
+++ app/api/route.js
@@ -0,0 +1,11 @@
+
+import { NextResponse } from "next/server";
+
+export async function GET(request) {
+  try {
+    return NextResponse.json("Hello World", { status: 200 });
+  } catch (error) {
+    console.log("error is", error);
+    return NextResponse.json("Error", { status: 500 });
+  }
+}
