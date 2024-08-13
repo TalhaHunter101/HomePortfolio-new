cf57f67b0902553b62050f9e9659cc148a171a58
fariqfarooq <fariq4949@gmail.com>
2024-08-13 23:00:24 +0530

    four cards added in property page & minor style changes in property page

---
+++ components/PropertyPageCards/locationCard.jsx
@@ -0,0 +1,23 @@
+'use client';
+import React from 'react';
+import { Card, CardBody, CardHeader } from "@nextui-org/react";
+
+export function LocationCard ({title })  {
+  return (
+    <Card className="m-4" style={{ minHeight: '150px' }}>
+      <CardHeader>
+        <h2 className="text-xl font-bold">{title}</h2>
+      </CardHeader>
+      <CardBody>
+      <div className="px-16 py-5 bg-gray-100 border border-gray-200 rounded-md">
+        <p className="text-[12px] font-bold text-base mb-3">Location</p>
+        <p className="text-[12px] font-medium">
+          1906 Brentwood ST, San Francisco, CA 94107
+        </p>
+      </div>
+      </CardBody>
+    </Card>
+  );
+};
+
+
