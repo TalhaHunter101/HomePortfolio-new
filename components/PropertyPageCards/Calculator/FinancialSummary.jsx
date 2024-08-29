baf21bf19ac24efb2c51897c3246c0a593387ae5
Chandrapalsingh <74819484+Chandrapalsingh12@users.noreply.github.com>
2024-08-29 10:08:17 +0530

    Added calculation

--- components/PropertyPageCards/Calculator/FinancialSummary.jsx
+++ components/PropertyPageCards/Calculator/FinancialSummary.jsx
@@ -1,18 +1,13 @@
 export default function FinancialSummary() {
     return (
       <div className="mt-4 mx-4">
-        <div className="flex justify-between items-center">
-          <span className="text-lg font-bold text-purple-900">
-            Upfront Equity
-          </span>
-          <span className="text-lg font-bold text-black">$664,612</span>
-        </div>
+       
         <div className="flex justify-between items-center mt-2 my-4">
           <span className="text-lg font-bold text-purple-900">
             Net Operating Income
           </span>
           <span className="text-lg font-bold text-black">
-            $379,637/yr &#x2022; $31,636/mo
+            £379,637/yr &#x2022; £31,636/mo
           </span>
         </div>
         <div className="flex justify-between items-center mt-2">
@@ -20,7 +15,7 @@ export default function FinancialSummary() {
             Leveraged Net Cash Flow
           </span>
           <span className="text-lg font-bold text-black">
-            $196,221/yr &#x2022; $16,352/mo
+            £196,221/yr &#x2022; £16,352/mo
           </span>
         </div>
       </div>
