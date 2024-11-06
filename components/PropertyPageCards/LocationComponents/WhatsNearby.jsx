94759caeb96d7b198d47a81c07e9455cd36a7682
cleverdevcc <dev@codeclever.tech>
2024-11-06 14:45:45 +0500

    showing distance between property and nearby places

--- components/PropertyPageCards/LocationComponents/WhatsNearby.jsx
+++ components/PropertyPageCards/LocationComponents/WhatsNearby.jsx
@@ -156,11 +156,11 @@ const haversineDistance = (coords1, coords2) => {
             lon = element.lon;
           } else {
             lat = element.center.lat;
-            lon = element.center.lon;
+            lon = element.center.lon || element.center.lng;
           }
 
           const address = formatAddress(element.tags);
-          const distance = haversineDistance({ lat, lon }, { lat: center.lat, lon: center.lon });
+          const distance = haversineDistance({ lat, lon }, { lat: center.lat, lon: center.lng || center.lon});
 
           return {
             name: element.tags.name || "na",
@@ -168,9 +168,10 @@ const haversineDistance = (coords1, coords2) => {
             address: address,
             lat,
             lon,
-            // distance: `${distance.toFixed(2)} km`,
+            distance: !isNaN(distance) ? `${distance.toFixed(2)} km` : "N/A",
           };
-        });
+        })
+        .filter((location) => location !== null);
 
       return locations;
     } catch (error) {
