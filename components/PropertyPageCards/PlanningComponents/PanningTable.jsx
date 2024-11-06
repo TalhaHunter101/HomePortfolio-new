55f0fe05b0d6b26761e4dfcd3b3c68d708615629
Chandrapalsingh <74819484+Chandrapalsingh12@users.noreply.github.com>
2024-11-06 20:19:05 +0530

    fix planning

--- components/PropertyPageCards/PlanningComponents/PanningTable.jsx
+++ components/PropertyPageCards/PlanningComponents/PanningTable.jsx
@@ -63,10 +63,10 @@ const PlanningApplicationsTable = ({ planningData }) => {
       case "status":
         return (
           <Chip
-            color={statusColorMap[source?.other_fields?.decision] || "warning"}
+            color={statusColorMap[source?.app_state] || "warning"}
             variant="flat"
           >
-            {source?.other_fields?.decision || "Others"}
+            {source?.app_state || "Others"}
           </Chip>
         );
       case "address":
