4e7f0cc3d510cad35dcf9d99dafa17e9f305eacb
Chandrapalsingh <74819484+Chandrapalsingh12@users.noreply.github.com>
2024-11-16 15:09:33 +0530

    Added Stapper

--- app/calculator/[name]/page.jsx
+++ app/calculator/[name]/page.jsx
@@ -1,10 +1,58 @@
-import Steper1Calculator from '@/components/BrrCalculator/Steper1Calculator'
-import React from 'react'
+'use client';
+import React, { useState } from 'react';
+import HorizontalSteps from "@/components/BrrCalculator/Stepper";
+import Steper3Calculator from "@/components/BrrCalculator/Steper3Calculator";
 
 function Calculators() {
+  const [currentStep, setCurrentStep] = useState(0);
+
+  // Components for each step
+  const StepComponents = {
+    0: () => (
+      <p>Property Info</p>
+    ),
+    1: () => (
+      <p>Purchase Info</p>
+    ),
+    2: () => (
+      <Steper3Calculator />
+    ),
+    3: () => (
+      <p>Results</p>
+    ),
+  };
+
+  const handleStepChange = (step) => {
+    setCurrentStep(step);
+  };
+
   return (
-   <Steper1Calculator />
-  )
+    <div className="container px-4">
+      <div className="mt-20 flex justify-center">
+        <HorizontalSteps
+          currentStep={currentStep}
+          onStepChange={handleStepChange}
+          steps={[
+            {
+              title: "Property Info",
+            },
+            {
+              title: "Purchase Info",
+            },
+            {
+              title: "Rental Info",
+            },
+            {
+              title: "Results",
+            },
+          ]}
+        />
+      </div>
+
+      {/* Render current step component */}
+      {StepComponents[currentStep]()}
+    </div>
+  );
 }
 
-export default Calculators
+export default Calculators;
