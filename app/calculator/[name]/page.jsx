cbcf0ab9c47891efffb95242dc7f8d44e40829e8
Chandrapalsingh <74819484+Chandrapalsingh12@users.noreply.github.com>
2024-11-16 14:51:47 +0530

    Added Calculator

---
+++ app/calculator/[name]/page.jsx
@@ -0,0 +1,10 @@
+import Steper1Calculator from '@/components/BrrCalculator/Steper1Calculator'
+import React from 'react'
+
+function Calculators() {
+  return (
+   <Steper1Calculator />
+  )
+}
+
+export default Calculators
---
+++ components/BrrCalculator/Steper1Calculator.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Steper1Calculator() {
+  return (
+    <div>Steper1Calculator</div>
+  )
+}
+
+export default Steper1Calculator
